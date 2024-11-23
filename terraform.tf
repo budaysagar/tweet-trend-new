ed83fe7af2160e25a91f603aa6c7bb958b0df19a
budaysagar <busc1996@gmail.com>
2024-11-22 19:36:24 -0500

    terraform code changes

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.77.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+}
---
+++ v1-ec2.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-east-1"
+}
+provider "tls" {
+}
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.keypair.key_name
+  security_groups = [aws_security_group.ssh_access.name]
+
+  tags = {
+    Name        = "demo-server"
+    Environment = "test"
+  }
+}
+resource "tls_private_key" "demo-key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "keypair" {
+  key_name   = "my_key"
+  public_key = tls_private_key.demo-key.public_key_openssh
+}
+resource "local_file" "private_key_file" {
+  filename        = "C:/Users/ranad/cicdproject/terraform_code/my_key.pem"
+  content         = tls_private_key.demo-key.private_key_pem
+  file_permission = "0600" # Secure file permissions
+}
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh_access"
+  description = "Allow SSH access from anywhere"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # All traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
