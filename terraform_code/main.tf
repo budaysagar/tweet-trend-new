85a91e2744ee5fd022ad966418aee13afa30e522
budaysagar <busc1996@gmail.com>
2024-11-23 15:24:15 -0500

    changes in vpc

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "demo-server" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.keypair.key_name
   security_groups = [aws_security_group.ssh_access.name]
+  subnet_id = aws_subnet.public_subnet.id
 
   tags = {
     Name        = "demo-server"
@@ -31,6 +32,7 @@ resource "local_file" "private_key_file" {
 resource "aws_security_group" "ssh_access" {
   name        = "ssh_access"
   description = "Allow SSH access from anywhere"
+  vpc_id = aws_vpc.demo-vpc.id
 
   ingress {
     from_port   = 22
@@ -46,3 +48,54 @@ resource "aws_security_group" "ssh_access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "demo-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "demo_igw" {
+  vpc_id = aws_vpc.demo-vpc.id
+
+  tags = {
+    Name = "demo-igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.demo-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true # Auto-assign public IPs
+
+  tags = {
+    Name = "demo-public-subnet"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.demo-vpc.id
+
+  tags = {
+    Name = "demo-public-route-table"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.demo_igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
